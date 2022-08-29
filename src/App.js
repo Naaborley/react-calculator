aafa747fb892b40f1e8b844ec4ebcf85e2af908f
NAAborley <jemisco221@gmail.com>
2022-08-29 21:02:36 +0000

    add button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="zero text-right">0</h1>
+          </div>
+        </div>
+        <div className="row signs">
+          <div className="col-md-3">
+            <h1>AC</h1>
+          </div>
+          <div className="col-md-3">
+            <h1>+/-</h1>
+          </div>
+          <div className="col-md-3">
+            <h1>%</h1>
+          </div>
+          <div className="col-md-3 division">
+            <h1>÷</h1>
+          </div>
+        </div>
+        <div className="row no-1">
+          <div className="col-md-3">
+            <h1>7</h1>
+          </div>
+          <div className="col-md-3">
+            <h1>8</h1>
+          </div>
+          <div className="col-md-3">
+            <h1>9</h1>
+          </div>
+          <div className="col-md-3 division">
+            <h1>x</h1>
+          </div>
+        </div>
+        <div className="row no-2">
+          <div className="col-md-3">
+            <h1>4</h1>
+          </div>
+          <div className="col-md-3">
+            <h1>5</h1>
+          </div>
+          <div className="col-md-3">
+            <h1>6</h1>
+          </div>
+          <div className="col-md-3 division">
+            <h1>-</h1>
+          </div>
+        </div>
+        <div className="row no-3">
+          <div className="col-md-3">
+            <h1>1</h1>
+          </div>
+          <div className="col-md-3">
+            <h1>2</h1>
+          </div>
+          <div className="col-md-3">
+            <h1>3</h1>
+          </div>
+          <div className="col-md-3 division">
+            <h1>+</h1>
+          </div>
+        </div>
+        <div className="row no-4">
+          <div className="col-md-6">
+            <h1 className="text-center">0</h1>
+          </div>
+          <div
+            className="col-md-3 final-row"
+            style={{ backgroundColor: "#f0f0f0" }}
+          >
+            <h1>.</h1>
+          </div>
+          <div
+            className="col-md-3 final-row"
+            style={{ backgroundColor: "blueviolet", color: "white" }}
+          >
+            <h1>=</h1>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
